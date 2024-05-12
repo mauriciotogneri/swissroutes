b8dfcf3ceb9f68c1d1bce08e26470c8ddc4a9056
Mauricio Togneri <mauricio.togneri@gmail.com>
2024-05-12 22:49:30 +0200

    Improvements

--- bin/downloader.js
+++ bin/downloader.js
@@ -1,9 +1,36 @@
 const fs = require('fs')
 
 async function run() {
-  const json = await getFile('https://map.schweizmobil.ch/api/4/query/featuresmultilayers?attributes=yes&translated=true&language=en&MtblandRoutenNational=1')
+  await downloadGroup('Mtbland', 'mountainbiking')
+  await downloadGroup('Veloland', 'cycling')
+  await downloadGroup('Wanderland', 'hiking')
+}
+
+async function downloadGroup(group, folder) {
+  for (let i = 1; i < 10; i++) {
+    await downloadType(i, group, 'RoutenNational', `${folder}/national`)
+  }
+
+  for (let i = 1; i < 100; i++) {
+    await downloadType(i, group, 'RoutenRegional', `${folder}/regional`)
+  }
+
+  for (let i = 1; i < 1000; i++) {
+    await downloadType(i, group, 'RoutenLokal', `${folder}/local`)
+  }
+}
+
+async function downloadType(id, group, type, folder) {
+  console.log(`${group}-${type}: ${id}`)
+  const url = `https://map.schweizmobil.ch/api/4/query/featuresmultilayers?attributes=yes&translated=true&language=en&${group}${type}=${id}`
+  const filePath = `output/${folder}/${id}.json`
+  await downloadFile(url, filePath, false)
+}
+
+async function downloadFile(url, filePath, isPoint) {
+  const json = await getFile(url)
   const content = JSON.stringify(json, null, 4)
-  fs.writeFileSync('test.json', content, 'utf-8')
+  fs.writeFileSync(filePath, content, 'utf-8')
 }
 
 async function getFile(url) {
