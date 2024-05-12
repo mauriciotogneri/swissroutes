70183dce00d53dd070e56bdfc8d77b28ea651e3d
Mauricio Togneri <mauricio.togneri@gmail.com>
2024-05-12 22:58:32 +0200

    Improvements

--- bin/downloader.js
+++ bin/downloader.js
@@ -81,28 +81,23 @@ function writeFile(filePath, json) {
 }
 
 function getPoint(y, x) {
-  /*float y_aux = (y - 600000) / 1000000f
-  float x_aux = (x - 200000) / 1000000f
+  const y_aux = (y - 600000) / 1000000
+  const x_aux = (x - 200000) / 1000000
 
-  double lng = (2.6779094
-          + 4.728982 * y_aux
-          + 0.791484 * y_aux * x_aux
-          + 0.1306 * y_aux * Math.pow(x_aux, 2)
-          - 0.0436 * Math.pow(y_aux, 3)) * 100 / 36d
-  double lat = (16.9023892
-          + 3.238272 * x_aux
-          - 0.270978 * Math.pow(y_aux, 2)
-          - 0.002528 * Math.pow(x_aux, 2)
-          - 0.0447 * Math.pow(y_aux, 2) * x_aux
-          - 0.0140 * Math.pow(x_aux, 3)) * 100 / 36d
+  const lng = (2.6779094
+    + 4.728982 * y_aux
+    + 0.791484 * y_aux * x_aux
+    + 0.1306 * y_aux * Math.pow(x_aux, 2)
+    - 0.0436 * Math.pow(y_aux, 3)) * 100 / 36
 
-  JsonArray array = new JsonArray()
-  array.add(lat)
-  array.add(lng)
+  const lat = (16.9023892
+    + 3.238272 * x_aux
+    - 0.270978 * Math.pow(y_aux, 2)
+    - 0.002528 * Math.pow(x_aux, 2)
+    - 0.0447 * Math.pow(y_aux, 2) * x_aux
+    - 0.0140 * Math.pow(x_aux, 3)) * 100 / 36
 
-  return array*/
-
-  return [0, 0]
+  return [lat, lng]
 }
 
 run().catch(console.error)
