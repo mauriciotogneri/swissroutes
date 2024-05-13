2eaa55d6fd1ff0125e711cea527c3b820ea74839
Mauricio Togneri <mauricio.togneri@gmail.com>
2024-05-13 20:35:01 +0200

    Improvements

--- bin/other-downloader.js
+++ bin/other-downloader.js
@@ -1,8 +1,8 @@
 const helpers = require('./helpers.js')
 
 async function run() {
-  //await helpers.downloadPoint('other', 'serviceshop', 'Cycleservice')
-  //await helpers.downloadPoint('other', 'sightseeing', 'Sightseeing')
+  await helpers.downloadPoint('other', 'serviceshop', 'Cycleservice')
+  await helpers.downloadPoint('other', 'sightseeing', 'Sightseeing')
   await helpers.downloadPoint('other', 'mountainhike', 'MountainHiking')
 }
 
